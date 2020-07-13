49505606d3116c16ed30a0b809df64da19335142
JonuKlyain <55921224+JonuKlyain@users.noreply.github.com>
2020-07-13 21:43:03 +0300

    new site

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header-menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $(window).resize(function(event) {
+        adaptive_function();
+    });
+    function adaptive_header(w,h) {
+        var headerMenu=$('.header-menu');
+        var headerLang=$('.header-top-lang');
+        if(w<767){
+            if(!headerLang.hasClass('done')){
+                headerLang.addClass('done').appendTo(headerMenu);
+            }
+        }else{
+            if(headerLang.hasClass('done')){
+                headerLang.removeClass('done').prependTo($('.header-top'));
+            }
+        }
+        if(w<767){
+            if(!$('.header-bottom-menu').hasClass('done')){
+                $('.header-bottom-menu').addClass('done').appendTo(headerMenu);
+            }
+        }else{
+            $.each($('.header-bottom-menu'), function(index, val){
+                if($(this).hasClass('header-bottom-menu--right')){
+                    if($(this).hasClass('done')){
+                        $(this).removeClass('done').prependTo($('.header-bottom__column').eq(2));
+                    }
+                }else{
+                    if($(this).hasClass('done')){
+                    $(this).removeClass('done').prependTo($('.header-bottom__column').eq(0));
+                    }
+                }
+            });
+        }
+    }
+        function adaptive_function() {
+            var w=$(window).outerWidth();
+            var h=$(window).outerHeight();
+            adaptive_header(w,h);
+        }
+            adaptive_function();
+    function ibg(){
+
+        $.each($('.ibg'), function(index, val) {
+        if($(this).find('img').length>0){
+        $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+        });
+        }
+        
+        ibg();
+
+      
+});
